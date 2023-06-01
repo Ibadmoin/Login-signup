4974cccbefa87d812e256a39fa195945e81a62bb
Ibad Moin <123115847+Ibadmoin@users.noreply.github.com>
2023-06-01 18:48:51 +0500

    validation for signup page completed.
    Complete & responsive Login & signup page with validation using regex.
    note: user Data is storing in localStorage not in DataBase.

--- login-signup/app.js
+++ login-signup/app.js
@@ -50,66 +50,108 @@
 /// for multiple users
 
 
-function signup(){
-
+function signUp(){
     const email = document.getElementById("signEmail").value;
-    const pass = document.getElementById('signPass').value;
-
-    if(validateEmail(email)&& validatePassword(pass)){
+    const password = document.getElementById("signPass").value;
+    const uName = document.getElementById("signName").value;
+    if(validateEmail(email) && validatePassword(password)&& validateName(uName)){
         const userDate = {
-            email: email,
-            password: pass,
+            email:email,
+            password:password,
+            username:uName,
         };
-        saveUserToLocalStorage(userDate);
-        alert('Signup Successfull!');
+        saveToLocalStorage(userDate);
+
+        //sweet alert & redirection to login page
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Signup Successful!',
+            text: 'You can now proceed to login.',
+            confirmButtonText: 'Okay'
+        }).then( () => {
+            window.location.href = "index.html";
+        }) 
+   
     } else {
-        alert('Please enter a valid email and password.');
+        //sweet alert for invalid dteails
+        Swal.fire({
+            icon: 'error',
+            title: 'Invalid Details!',
+            text: 'Please enter valid email, password, & name',
+            confirmButtonText: 'Okay'
+        });
     }
-    console.log(email,pass);
+    console.log(email,password,uName);
+
+
+
 }
 
-function saveUserToLocalStorage(userDate){
-    let users = JSON.parse(localStorage.getItem('users')) ||[];
+
+function saveToLocalStorage(userDate){
+    let users = JSON.parse(localStorage.getItem('users')) || [];
     users.push(userDate);
-    localStorage.setItem("users", JSON.stringify(users));
-
-
+    localStorage.setItem("users",JSON.stringify(users));
 }
-
+// validation for emails
 
 function validateEmail(email){
     const emailRegex = /[!@#$%^&*()]/;
     return emailRegex.test(email);
 }
+// validation for passwords
 
-function validatePassword(pass){
-    const alphaRegex = /[a-zA-Z]/;
+function validatePassword(password){
     const numRegex = /[0-9]/;
+    const alphaRegex = /[a-zA-z]/;
     const symbolRegex = /[!@#$%^&*()]/;
 
-    const hasNumber = numRegex.test(pass);
-    const hasAlpha = alphaRegex.test(pass);
-    const hasSymbol = symbolRegex.test(pass);
+    const hasNumber = numRegex.test(password);
+    const hasAlpha = alphaRegex.test(password);
+    const hasSymbol = symbolRegex.test(password);
 
-    return hasNumber && hasAlpha && hasSymbol && pass.length >= 8;
-
+    return  hasNumber && hasSymbol && hasAlpha && password.length >= 8;
+}
+// validation for usernames
+function validateName(uName){
+    const nameRegex = /[a-zA-Z]/;
+    const hasvalidatename = nameRegex.test(uName);
+    const isNotEmpty = uName.trim().length > 0;
+    
+    return hasvalidatename && isNotEmpty;
+    
 }
 
 
-function signin(){
+// login via localstorage
+
+function signIn(){
     var loginEmail = document.getElementById("email").value;
     var loginPass = document.getElementById("password").value;
-
+    
     const users = JSON.parse(localStorage.getItem("users")) || [];
-    const matcheduser = users.find(user => user.email === loginEmail && user.password === loginPass );
-    if(matcheduser){
-        alert("Access Granted!");
+    const matchedUser = users.find(user => user.email === loginEmail && user.password === loginPass);
+    if(matchedUser){
+        // alert("Access");
+        // sweet alert for acepting request
+        Swal.fire({
+            icon: 'success',
+            title: 'Access Granted!',
+            showConfirmButton: false,
+            timer: 2500
+        });
     } else {
-        alert("Access denied@!");
+        // alert("denied");
+        // sweet alert for rejecting request
+        Swal.fire({
+            icon: 'error',
+            title: 'Access Denied!',
+            text: 'Invalid email or password',
+            confirmButtonText: 'OK'
+        });
     }
 }
-
-
 
 // placeholder anime
 
@@ -127,3 +169,22 @@ document.getElementById("password").addEventListener("focus",function(){
 document.getElementById("password").addEventListener("blur",function(){
     this.placeholder = "Type your password";
 })
+
+
+
+
+
+// create new account page redirect
+
+function signUpPage(){
+
+    window.location.href = "signup.html";
+
+}
+//backto login page redirect
+
+function loginPage(){
+
+    window.location.href="index.html";
+}
+
